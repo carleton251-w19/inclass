d5be471102d9754693d57d8f1517b03ed49c2ebe
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-01-09 13:40:31 -0600

    Scripted auto-commit on change (2019-01-09 13:40:31) by gitwatch.sh

--- day2/schemeiscool.rkt
+++ day2/schemeiscool.rkt
@@ -16,3 +16,14 @@
         0
         (+ 1 (list-length (cdr lst))))))
 (list-length '(a b c e g q))
+
+;; See if something is a list of numbers
+(define list-of-numbers?
+  (lambda (lst)
+    (if (null? lst)
+        #t
+        (and (number? (car lst))
+             (list-of-numbers? (cdr lst))))))
+
+(list-of-numbers? '(65 2 2 6 1))
+(list-of-numbers? '(65 2 2 a 1))
