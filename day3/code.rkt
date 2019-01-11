ff14158586f71ca230b9c2a4034ea00c0739b5f2
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-01-11 13:56:31 -0600

    Scripted auto-commit on change (2019-01-11 13:56:31) by gitwatch.sh

--- day3/code.rkt
+++ day3/code.rkt
@@ -1,4 +1,17 @@
 (require rackunit)
 
+(define remove-first
+  (lambda (item lst)
+    lst))
+
 (check-equal?
  (remove-first 'a '(a b c)) '(b c))
+
+(check-equal?
+ (remove-first 'b '(a b c)) '(a c))
+
+(check-equal?
+ (remove-first 'g '(a b c)) '(a b c))
+
+(check-equal?
+ (remove-first 'a '()) '())
