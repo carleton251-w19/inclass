41cfa6a16088d6582f75102f32b51710d2c7a7ce
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-01-14 12:44:10 -0600

    Scripted auto-commit on change (2019-01-14 12:44:10) by gitwatch.sh

---
+++ day4/examples.rkt
@@ -0,0 +1,6 @@
+(define subst
+  (lambda (old new input)
+    (cond [(null? input)  _____________]
+          [(list? input)  _____________]
+          [(equal?  ____  _____)   __________]
+          [else ______________])))
