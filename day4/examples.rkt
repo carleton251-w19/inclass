9a18024ee746ec8442f2b8c0dca57813d5281f46
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-01-14 13:33:50 -0600

    Scripted auto-commit on change (2019-01-14 13:33:50) by gitwatch.sh

--- day4/examples.rkt
+++ day4/examples.rkt
@@ -37,3 +37,15 @@
 
 
 
+
+
+
+
+(define gen-lazy-list
+  (lambda (start stop)
+    (if (> start stop)
+        #f
+        (cons start
+            (lambda () (gen-lazy-list (+ start 1) stop))))))
+
+
