403ee50ea14029635da63414078ae81937bfe360
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-01-21 13:05:00 -0600

    Scripted auto-commit on change (2019-01-21 13:05:00) by gitwatch.sh

---
+++ day7/wrapper_example2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Wrapper {
+   int value;
+};
+
+int main() {
+   struct Wrapper *w1 = malloc(sizeof(struct Wrapper));
+   struct Wrapper *w2 = malloc(sizeof(struct Wrapper));
+   (*w1).value = 42;
+   w2 = w1;
+   printf("%i %i\n",(*w1).value,(*w2).value);
+   (*w2).value = 50;
+   printf("%i %i\n",(*w1).value,(*w2).value);
+}
