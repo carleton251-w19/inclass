b45231446334b5526b3644253c838d417350a9bc
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-01-21 13:27:09 -0600

    Scripted auto-commit on change (2019-01-21 13:27:09) by gitwatch.sh

---
+++ day7/more.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+struct Wrapper {
+   int value;
+};
+
+int main() {
+    int a = 12;
+    int *x;
+    x = &a;    // get me the address of a where it is in memory
+    *x = 6;
+    printf("%i\n",*x);
+    printf("%i\n",a);
+    int *y;
+    y = x;
+    printf("%i\n",*y);
+    printf("%i\n",a);
+    *x = 12;
+    printf("%i\n",*x);
+    printf("%i\n",*y);
+    printf("%i\n",*a);
+}
+    
