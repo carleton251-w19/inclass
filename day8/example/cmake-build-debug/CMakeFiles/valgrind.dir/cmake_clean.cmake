a741c34c02b2bdb031b8772ddd494e784b8ddab0
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-01-23 13:35:35 -0600

    Scripted auto-commit on change (2019-01-23 13:35:35) by gitwatch.sh

---
+++ day8/example/cmake-build-debug/CMakeFiles/valgrind.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/valgrind.dir/valgrind.c.o"
+  "valgrind.pdb"
+  "valgrind"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/valgrind.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
