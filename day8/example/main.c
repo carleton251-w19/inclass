c624cbe1b1d0599ab33c264ea14283201050f584
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-01-23 13:02:00 -0600

    Scripted auto-commit on change (2019-01-23 13:02:00) by gitwatch.sh

--- day8/example/main.c
+++ day8/example/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+double fun1(int x) {
+    return x * 0.5;
+}
+
+
+double fun2(int *y) {
+    *y = *y * 2;
+    return *y * 1.5;
+}
+
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int x = 4;
+    printf("x = %i\n",x);
+    double a = fun1(x);
+    printf("x = %i\n",x);
+    printf("a = %g\n",a);
+
+    int *z = malloc(sizeof(int));
+    *z = 8;
+    printf("z = %i\n",*z);
+    double b = fun2(z);
+    printf("z = %i\n",*z);
+    printf("b = %g\n",b);
 }
