94193a7c3c9687e5436fff327e30fb484d7cdbdf
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-01-30 12:40:48 -0600

    Scripted auto-commit on change (2019-01-30 12:40:48) by gitwatch.sh

--- day11/example/main.c
+++ day11/example/main.c
@@ -4,17 +4,5 @@
 
 
 int main() {
-    int x = 4;
-    printf("x = %i\n",x);
-    double a = fun1(x);
-    printf("x = %i\n",x);
-    printf("a = %g\n",a);
-
-    int *z = malloc(sizeof(int));
-    *z = 8;
-    printf("z = %i\n",*z);
-    double b = fun2(z);
-    printf("z = %i\n",*z);
-    printf("b = %g\n",b);
-    free(z);
+ 
 }
